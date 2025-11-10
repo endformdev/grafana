e7cce146e97a70d006952e859939538c9a2e446f
Jakob Norlin <jakob.stahl91@gmail.com>
2025-11-10 10:20:14 +0100

    Pass env var through

--- pkg/build/e2e-playwright/e2e.go
+++ pkg/build/e2e-playwright/e2e.go
@@ -25,6 +25,7 @@ type RunTestOpts struct {
 	TestResultsExportDir string
 	PlaywrightCommand    string
 	CloudPluginCreds     *dagger.File
+	EndformAPIKey        string
 }
 
 func RunTest(
@@ -54,6 +55,10 @@ func RunTest(
 	if opts.CloudPluginCreds != nil {
 		fmt.Println("DEBUG: CloudPluginCreds file is provided, mounting to /tmp/outputs.json")
 		e2eContainer = e2eContainer.WithMountedFile("/tmp/outputs.json", opts.CloudPluginCreds)
+	}
+
+	if opts.EndformAPIKey != "" {
+		e2eContainer = e2eContainer.WithEnvVariable("ENDFORM_API_KEY", opts.EndformAPIKey)
 	}
 
 	e2eContainer = e2eContainer.WithExec(playwrightCommand, dagger.ContainerWithExecOpts{
